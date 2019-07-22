029c6fe96f810d80ca51a4de6a8ef7660023e5bb
Poli <diegoroca.ordes@gmail.com>
2019-07-22 23:55:12 +0200

    Jump
    Iteration 3: Simple jump and acceleration based on simple physics

--- Assets/Scripts/Player2D.cs
+++ Assets/Scripts/Player2D.cs
@@ -7,20 +7,39 @@ public class Player2D : MonoBehaviour
 {
     Controller2D m_Controller;
     Vector3 m_Velocity;
+    float m_JumpVelocity;
+    float m_CurrentVelocity;
 
     public float Gravity;
     public float MoveSpeed;
+    public float JumpHeight;
+    public float JumpTime;
+    public KeyCode JumpKey;
+    public float AccelTimeAir;
+    public float AccelTimeGround;
 
     void Start()
     {
         m_Controller = GetComponent<Controller2D>();
+
+        //Resolvemos la gravedad y velocidad del salto deseadas con la altura y el tiempo
+        //dx = vi * t + (a*t^2)/2
+        //variacion movimiento = velocidad inicial * tiempo + (aceleracion * tiempo^2)/2
+        //
+        //v(t) = vi+a*t 
+        //La velocidad en un punto de tiempo concreto es la velocidad inicial + aceleracion * tiempo
+
+        Gravity = -(2 * JumpHeight) / Mathf.Pow(JumpTime, 2);
+        m_JumpVelocity = Mathf.Abs(Gravity * JumpTime);
     }
 
     void Update()
     {
         //Si no estamos en terreno resetea la gravedad
         if (m_Controller.getCollisions().above || m_Controller.getCollisions().below)
+        {
             m_Velocity.y = 0;
+        }
 
         m_Velocity.y += Gravity * Time.deltaTime;
         GetInput();
@@ -30,6 +49,15 @@ public class Player2D : MonoBehaviour
     void GetInput()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        m_Velocity.x = input.x * MoveSpeed;
+        
+        if (Input.GetKeyDown(JumpKey) && m_Controller.getCollisions().below)
+        {
+            m_Velocity.y = m_JumpVelocity;
+        }
+        float targetVelocityX = input.x * MoveSpeed;
+
+        //Cambiamos la aceleracion si estamos en suelo o aire
+        m_Velocity.x = Mathf.SmoothDamp(m_Velocity.x, targetVelocityX, ref m_CurrentVelocity, 
+            m_Controller.getCollisions().below?AccelTimeGround:AccelTimeAir);
     }
 }
